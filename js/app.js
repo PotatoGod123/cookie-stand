0f72f2432441a0176b213ee279a2095044acf0b7
Cristian Robles <formymoment12@gmail.com>
2020-11-02 16:26:21 -0600

    Added index info and started app for sale caculations

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
 'use strict';
 
+
+var seatleStore = {
+  storeName: 'Seattle',
+  minCustormers: 23,
+  maxCustormers: 62,
+  averageCookieSale: 6.3,
+  averageMath: function(min,max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
+  },
+  actualHours: function(){
+    return this.averageMath(this.minCustormers,this.maxCustormers);
+  }
+
+};
